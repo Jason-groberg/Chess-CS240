d0375e4c1b1ce0d1b31b9eea62036a1c3661dc16
Jason <jasongroberg@gmail.com>
2026-02-05 16:51:17 -0700

    Pawn Moves calculated in the same function, for both colors, fixed nesting length issues.

--- shared/src/main/java/chess/Testing.java
+++ shared/src/main/java/chess/Testing.java
@@ -6,7 +6,10 @@ public class Testing {
 
     public static void main(String[] args) {
         ChessGame game = new ChessGame();
+        game.setTeamTurn(ChessGame.TeamColor.WHITE);
         ChessBoard board = game.getBoard();
         System.out.println(board);
+        ChessPosition e2 = new ChessPosition(2,2);
+        ChessPosition e4 = new ChessPosition(4,2);
     }
 }
