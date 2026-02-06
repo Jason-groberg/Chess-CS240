bdd5e357280f0b2b6c768468f7d85e967d5764d4
Jason <jasongroberg@gmail.com>
2026-02-06 01:52:11 -0700

    castle logic implemented so that castle moves are returned, and can be made during the game.

--- shared/src/main/java/chess/PieceMoves.java
+++ shared/src/main/java/chess/PieceMoves.java
@@ -65,28 +65,6 @@ public class PieceMoves {
         return legalMoves;
     }
 
-    public boolean checkCastle(ChessPosition kingPos,ChessBoard board, Collection<ChessMove> kingMoves, Collection<ChessMove> rookMoves){
-        ChessGame.TeamColor white = ChessGame.TeamColor.WHITE;
-        ChessGame.TeamColor black = ChessGame.TeamColor.BLACK;
-        ChessPiece king = board.getPiece(kingPos);
-        ChessGame.TeamColor kingColor = king.getTeamColor();
-        ChessPiece.PieceType rook = ChessPiece.PieceType.ROOK;
-        ChessPosition whiteKingStart = new ChessPosition(1,5);
-        ChessPosition whiteRook2Start = new ChessPosition(1,8);
-        ChessPosition whiteKnight2Start = new ChessPosition(1,7);
-        ChessPosition whiteBishop2Start = new ChessPosition(1,6);
-        if(board.getPiece(whiteKingStart).equals(king) && board.getPiece(whiteRook2Start).getPieceType() == rook
-                && board.getPiece(whiteKnight2Start)==null && board.getPiece(whiteBishop2Start)==null){
-            ChessMove rookMove = new ChessMove(whiteRook2Start, whiteBishop2Start, null);
-            ChessMove kingMove = new ChessMove(kingPos,whiteKnight2Start,null );
-            rookMoves.add(rookMove);
-            kingMoves.add(kingMove);
-            return true;
-        }
-        return false;
-    }
-
-
     public void pawnPromotion(ChessPosition startPosition, ChessPosition promotionSquare, Collection<ChessMove> pawnMoves) {
         EnumSet<ChessPiece.PieceType> types = EnumSet.of(ChessPiece.PieceType.ROOK, ChessPiece.PieceType.QUEEN,
                 ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP);
