6b94aee3699f1ca1770ae4444f74f5c829afafe3
jason <jasongroberg@gmail.com>
2026-03-20 12:40:18 -0600

    Header for black/white perspective added to correctly draw header.

--- client/src/main/java/ui/DrawChessBoard.java
+++ client/src/main/java/ui/DrawChessBoard.java
@@ -1,8 +1,9 @@
 package ui;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class DrawChessBoard extends EscapeSequences {
-
-
     private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_PADDED_CHARS = 3;
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
@@ -17,6 +18,7 @@ public class DrawChessBoard extends EscapeSequences {
     private static final String WHITE_KNIGHT = EscapeSequences.WHITE_KNIGHT;
     private static final String WHITE_ROOK = EscapeSequences.WHITE_ROOK;
     private static final String WHITE_PAWN = EscapeSequences.WHITE_PAWN;
+    private static final String[] whiteFiles = new String[] {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
 
     //Black Pieces
     private static final String BLACK_KING = EscapeSequences.BLACK_KING;
@@ -25,6 +27,128 @@ public class DrawChessBoard extends EscapeSequences {
     private static final String BLACK_KNIGHT = EscapeSequences.BLACK_KNIGHT;
     private static final String BLACK_ROOK = EscapeSequences.BLACK_ROOK;
     private static final String BLACK_PAWN = EscapeSequences.BLACK_PAWN;
+    private static final String[] blackFiles = new String[]{" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "};
+
+    public static void main(String[] args){
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(ERASE_SCREEN);
+
+        drawHeaders(out, false);
+
+        //drawChessBoard(out, true);
+
+    }
+
+    private static void drawHeaders(PrintStream out, boolean isWhite){
+        setHeader(out);
+        out.print("   ");
+        String[] files;
+        if(isWhite){files = whiteFiles;}
+        else{files = blackFiles;}
+
+        for (String file : files){
+            out.print(file);
+        }
+
+        out.print("   ");
+        out.println();
+    }
+
+    private static void drawChessBoard(PrintStream out, boolean isWhite) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+
+            drawRowOfSquares(out);
+
+            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+                // Draw horizontal row separator.
+                drawHorizontalLine(out);
+                setBlack(out);
+            }
+        }
+    }
+
+
+    private static void drawRowOfSquares(PrintStream out) {
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                setWhite(out);
+
+                if (squareRow == SQUARE_SIZE_IN_PADDED_CHARS / 2) {
+                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+
+                    out.print(EMPTY.repeat(prefixLength));
+                    out.print(EMPTY.repeat(suffixLength));
+                }
+                else {
+                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
+                }
+
+                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                    // Draw vertical column separator.
+                    setRed(out);
+                    out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+                }
+
+                setBlack(out);
+            }
+
+            out.println();
+        }
+    }
+
+    private static void printPlayer(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_BLACK);
+
+        out.print(player);
+
+        setWhite(out);
+    }
+
+
+    private static void drawHorizontalLine(PrintStream out) {
+
+        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
+                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
+
+        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+            setRed(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+
+            setBlack(out);
+            out.println();
+        }
+    }
+
+
+    private static void setHeader(PrintStream out){
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_WHITE);
+        out.print(SET_TEXT_ITALIC);
+        out.print(SET_TEXT_BOLD);
+    }
+
+    private static void setRed(PrintStream out){
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setGreen(PrintStream out) {
+        out.print(SET_BG_COLOR_DARK_GREEN);
+        out.print(SET_TEXT_COLOR_DARK_GREY);
+    }
 
 
 
